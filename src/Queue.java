5066d457822eb3aa64dc6aada2e8c0ddff2e6e05
rich7310 <rich7310@ISS2V1RKH2.scdsb.pri>
2017-12-18 14:06:14 -0500

    Created Stack and Queue Files

---
+++ src/Queue.java
@@ -0,0 +1,9 @@
+public interface Queue{
+	public Object peekFront();
+	public Object dequeue();
+	public void enqueue(Object o);
+	public int size();
+}
+
+
+
---
+++ src/Stack.java
@@ -0,0 +1,9 @@
+
+public interface Stack{
+	public Object peekTop();
+	public Object pop();
+	public void push(Object o);
+	public int size();
+}
+
+
